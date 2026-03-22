e146a39130991a62574de4bc5f897213d18d80b6
bilalpvt2030 <bilalahmedpvt2030@gmail.com>
2026-03-22 09:03:21 +0530

    feat(phase4): add Projects link to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,6 +7,7 @@ const links = [
   { name: 'About', href: '#about' },
   { name: 'Skills', href: '#skills' },
   { name: 'Experience', href: '#experience' },
+    { name: 'Projects', href: '#projects' },
   { name: 'Contact', href: '#contact' },
   { name: 'Pursuits', href: '#pursuits' },
 ];
